b80866b967756caedd4beafd3815bd261366e83f
Toby Inkster <mail@tobyinkster.co.uk>
2011-10-01 18:40:42 +0000

    Ubercool RDF-driven build system.
    --HG--
    branch : Module-Install-RDF
    extra : convert_revision : svn%3A3fe8e991-6959-4966-b76d-b07eca2b6e37/Module-Install-RDF%40953

---
+++ lib/Module/Install/Admin/RDF.pm
@@ -0,0 +1,40 @@
+package Module::Install::Admin::RDF;
+
+use 5.008;
+use base qw(Module::Install::Base);
+use strict;
+
+use RDF::Trine qw[];
+use Scalar::Util qw[];
+use URI::file qw[];
+
+our $VERSION = '0.001';
+
+my $Model = {};
+
+sub rdf_metadata
+{
+	my ($self) = @_;
+	
+	my $addr = Scalar::Util::refaddr($self->_top);
+	return $Model->{$addr} if defined $Model->{$addr};
+	my $model = $Model->{$addr} = RDF::Trine::Model->new;
+	
+	my $turtle = RDF::Trine::Parser->new('Turtle');
+	while (<meta/*.{ttl,turtle,nt}>)
+	{
+		my $iri = URI::file->new_abs($_);
+		$turtle->parse_file_into_model("$iri", $_, $model);
+	}
+
+	my $rdfxml = RDF::Trine::Parser->new('RDFXML');
+	while (<meta/*.{rdf,rdfxml,rdfx}>)
+	{
+		my $iri = URI::file->new_abs($_);
+		$rdfxml->parse_file_into_model("$iri", $_, $model);
+	}
+	
+	return $model;
+}
+
+1;
