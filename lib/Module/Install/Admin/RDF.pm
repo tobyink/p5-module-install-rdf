e71f55906aae8812742ae1e1cc419cffa422e80d
Toby Inkster <mail@tobyinkster.co.uk>
2013-03-16 12:58:09 +0000

    documentation
    --HG--
    branch : Module-Install-RDF

--- lib/Module/Install/Admin/RDF.pm
+++ lib/Module/Install/Admin/RDF.pm
@@ -139,3 +139,39 @@ sub write_meta_ttl
 }
 
 1;
+
+__END__
+=head1 NAME
+
+Module::Install::Admin::RDF - internals for Module::Install::RDF
+
+=head1 DESCRIPTION
+
+Code that only runs on the module author's machine.
+
+=head1 BUGS
+
+Please report any bugs to L<http://rt.cpan.org/>.
+
+=head1 SEE ALSO
+
+L<Module::Install::RDF>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011-2013 by Toby Inkster
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
--- lib/Module/Install/RDF.pm
+++ lib/Module/Install/RDF.pm
@@ -62,9 +62,15 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011-2012 by Toby Inkster
+Copyright (C) 2011-2013 by Toby Inkster
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 =cut
